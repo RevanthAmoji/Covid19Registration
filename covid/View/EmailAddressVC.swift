65311247a6d1b7abdbd349c799201d533a962dd7
Revanth Amoji <revanth.talk2@gmail.com>
2020-12-21 18:51:51 +0530

    progress removed

--- covid/View/EmailAddressVC.swift
+++ covid/View/EmailAddressVC.swift
@@ -33,7 +33,7 @@ class EmailAddressVC: UIViewController,ProgressBarShower{
     
     func checkConnectivity() {
         
-        self.showProgressBar()
+      //  self.showProgressBar()
         if Reachability.isConnectedToNetwork() {
             let email = emailTF.text ?? ""
             let dic = "{\"EmailAddress\":\"\(email)\",\"param\":'{\"EmailAddress\":\"\(email)\"}'}"
@@ -46,7 +46,7 @@ class EmailAddressVC: UIViewController,ProgressBarShower{
                 switch result {
                 
                 case .success(let dashboads):
-                    self.hideProgressBar()
+                   // self.hideProgressBar()
                     if dashboads.isSuccess ?? false {
                         
                             let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -59,13 +59,13 @@ class EmailAddressVC: UIViewController,ProgressBarShower{
                     }
                     
                 case .failure( _):
-                    self.hideProgressBar()
+                   // self.hideProgressBar()
                     //something went wrong, print the error.
                     self.showOfflineMessage(title: Endpoint.errorMessage, msg: "")
                 }
             })
         } else {
-            self.hideProgressBar()
+         //   self.hideProgressBar()
             self.showOfflineMessage(title: "Network Error", msg: "Unable to access the Network")
        }
     }
